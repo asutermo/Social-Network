baf2e327f6063eb185b42ac4d444e3ef458ebd60
asutermo <asutermo@mymail.mines.edu>
2012-11-28 00:17:13 -0700

    cleaned header up

--- util.php
+++ util.php
@@ -2,10 +2,9 @@
 
 function add_header(){
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link href="generic.css" type="text/css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="stylesheets/generic.css"
+	    title="generic" /> 
 <?php
 }
