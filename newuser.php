4827fc1166689e57677e171c73c177282f53b654
asutermo <asutermo@mymail.mines.edu>
2012-11-28 00:46:39 -0700

    almost done with submission form

--- newuser.php
+++ newuser.php
@@ -10,13 +10,27 @@
 		<h1>SIGN UP DO IT!!!</h1>
 		<div>
 			<form action="createuser.php" method="POST">
-				<label for="user">Username or Email</label>
+				<label for="user">Username</label>
+				<input type="text" name="user" id="user"/>
+				<br />
 				<label for="first">First name</label>
+				<input type="text" name="first" id="first"/>
+				<br />
 				<label for="last">Last name</label>
+				<input type="text" name="last" id="last"/>
+				<br />
 				<label for="email">Email</label>
+				<input type="email" name="email" id="email"/>
+				<br />
 				<label for="password">Password</label>
+				<input type="password" name="password" id="password"/>
+				<br />
 				<label for="rpassword">Repeat password</label>
+				<input type="password" name="rpassword" id="rpassword"/>
+				<br />
 				<label for="image">Upload Image</label>
+				<input type="file" name="image" id="image"/>
+				<br />
 				<label for="gender">Gender</label>
 				<label for="age">Age</label>
 				<label for="other">Other</label>
