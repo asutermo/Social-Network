dd623b40e28cd3be9f6ff7c61d4054ccc23190e6
asutermo <asutermo@mymail.mines.edu>
2012-12-02 15:00:30 -0700

    working on email signup validation

--- partials/signup.php
+++ partials/signup.php
@@ -2,6 +2,7 @@
 	session_start();
 
 	$username = strip_tags($_POST['user']);
+	$email = strip_tags($_POST['email']);
 	$firstname = strip_tags($_POST['first']);
 	$lastname = strip_tags($_POST['last']);
 	$password = strip_tags($_POST['password']);
@@ -9,4 +10,11 @@
 	$gender = strip_tags($_POST['gender']);
 	$age = strip_tags($_POST['age']);
 	$other = strip_tags($_POST['other']);
+
+	@ $db = new mysqli(localhost, root, '', team04);
+	//$db = new mysqli(localhost, team04, fuchsia, team04);
+
+	//check for pre-existing emails
+	$result = $db->query("SELECT * FROM users WHERE email='{$email}'");
+	$count = mysqli_num_rows($result);
 ?>
