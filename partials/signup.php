f3dfd597a6d73dec249f4f52134011fd463c08ba
asutermo <asutermo@mymail.mines.edu>
2012-12-02 14:50:13 -0700

    retrieving vars on signup

--- partials/signup.php
+++ partials/signup.php
@@ -1,4 +1,12 @@
 <?php
-session_start();
+	session_start();
 
+	$username = strip_tags($_POST['user']);
+	$firstname = strip_tags($_POST['first']);
+	$lastname = strip_tags($_POST['last']);
+	$password = strip_tags($_POST['password']);
+	$profilepic = strip_tags($_POST['image']);
+	$gender = strip_tags($_POST['gender']);
+	$age = strip_tags($_POST['age']);
+	$other = strip_tags($_POST['other']);
 ?>
