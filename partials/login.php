c1a3c336af7016cfcfb1ac17074b2f55b796aa7d
asutermo <asutermo@mymail.mines.edu>
2012-12-02 14:44:16 -0700

    finished tables on susan. now working on logging in and signing up

--- partials/login.php
+++ partials/login.php
@@ -1,1 +1,10 @@
-//$db = new mysqli(localhost, team04, fuchsia, team04);
+<?php
+	session_start();
+
+	$username = strip_tags($_POST['user']);
+	$password = md5(strip_tags($_POST['password']));
+	//$db = new mysqli(localhost, team04, fuchsia, team04.users);
+	@ $db = new mysqli(localhost, root, '', team04.users);
+
+
+?>
