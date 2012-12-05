92dc56967ad4d626cf60123a7e2521467bd0c54a
asutermo <asutermo@mymail.mines.edu>
2012-12-04 23:21:34 -0700

    database script updated

---
+++ database/generate_tables.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE  IF NOT EXISTS `team04`;
+USE `team04`;
+
+DROP TABLE IF EXISTS `user_friends`;
+CREATE TABLE `user_friends` (
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `user_friends` WRITE;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `user_statuses`;
+CREATE TABLE `user_statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_statuses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `user_statuses` WRITE;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_pic` blob,
+  `gender` varchar(1) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `other` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+UNLOCK TABLES;
