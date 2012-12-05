a28747072f234d25cf674bcb6a5e0b05f211357c
asutermo <asutermo@mymail.mines.edu>
2012-12-04 22:58:47 -0700

    fixed table on members page

--- partials/members.php
+++ partials/members.php
@@ -21,17 +21,17 @@
 
 		$result = $db->query("SELECT * FROM users");
 		$count = mysqli_num_rows($result);
-
-		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-			echo "<tr>";
-			echo "<td>".$row["username"]."</td>";
-			echo "<td>".$row["first_name"]."</td>";
-			echo "<td>".$row["last_name"]."</td>";
-			echo "<td>".$row["profile_pic"]."</td>";
-			echo "</tr>";
-			echo "<br />";
-			
-		}
+		echo "<table>";
+			while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+				echo "<tr>";
+				echo "<td>".$row["username"]."</td>";
+				echo "<td>".$row["first_name"]."</td>";
+				echo "<td>".$row["last_name"]."</td>";
+				echo "<td>".$row["profile_pic"]."</td>";
+				echo "</tr>";
+				
+			}
+		echo "</table>";
 		$db->close();
 		?>
 </div>
