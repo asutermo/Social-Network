d0c6064bda530fb830df93bcfadd0b48f55e7e38
asutermo <asutermo@mymail.mines.edu>
2012-12-08 22:24:45 -0700

    working on cleaning news feed up

--- home.php
+++ home.php
@@ -22,9 +22,20 @@
 		<h1>Welcome to your news feed</h1>
 		<div>
 			<table id="statuses">
+				<tr>
+					<th>Poster</th>
+					<th>Date posted</th>
+					<th>Status</th>
+				</tr>
 				<?php
 					foreach ($statuses as $status) {
 						echo "<tr>";
+						if (isset($status["username"])) {
+							echo "<td>".$status["username"]."</td>";	
+						}
+						else {
+							echo "<td>You</td>";	
+						}
 						$date = strtotime($status["post_date"]); 
 						echo "<td>".date("Y-m-d H:i", $date)."</td>";
 						echo "<td>".$status["status"]."</td>";
