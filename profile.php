62c8beabc043ed78c710c10f60fce534d1e13ec7
asutermo <asutermo@mymail.mines.edu>
2012-12-09 14:55:10 -0700

    working on proper profile pic upload

--- profile.php
+++ profile.php
@@ -44,11 +44,13 @@
 		<div id="profile_info">
 			<?php
 				foreach ($user_info as $user_information) {
+					echo "<span>Username: ".$user_information["username"]."</span><br />\n";
+					echo "<img src='",$user_information['profile_pic'],"' width='175' height='200' /><br />\n";
 					echo "<span>Username: ".$user_information["username"]."</span><br />";
-					echo "<span>Name: ".$user_information["first_name"]." ".$user_information["last_name"]."</span><br />";
-					echo "<span>Age: ".$user_information["age"]."</span><br />";
-					echo "<span>Gender: ".$user_information["gender"]."</span><br />";
-					echo "<span>Other Information: ".$user_information["other"]."</span><br />";
+					echo "<span>Name: ".$user_information["first_name"]." ".$user_information["last_name"]."</span><br />\n";
+					echo "<span>Age: ".$user_information["age"]."</span><br />\n";
+					echo "<span>Gender: ".$user_information["gender"]."</span><br />\n";
+					echo "<span>Other Information: ".$user_information["other"]."</span><br />\n";
 					echo "<br />";
 				}
 				
