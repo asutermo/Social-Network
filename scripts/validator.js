7878620b79eaace4cfa6725bb0b93fea903e8846
asutermo <asutermo@mymail.mines.edu>
2012-12-09 21:28:06 -0700

    working on form validation

--- scripts/validator.js
+++ scripts/validator.js
@@ -1,5 +1,40 @@
 //validate each field
 function validate_signup(form) {
+	var msg = "";
+	if (form.user.value == "" || form.user.value == undefined) {
+		msg += "Username does not work. <br />";
+	}
+	if (form.first.value == "" || form.first.value == undefined) {
+		msg += "Enter your first name. <br />";
+	}
+	if (form.user.value == "" || form.user.value == undefined) {
+		msg += "Enter your last name. <br />";
+	}
 
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	var address = form.email.value;
+	if(reg.test(address) == false) {
+		msg += "You must provide a valid email address. <br />";
+	}
 
+	if ((form.password.value == "" 
+		|| form.password.value == undefined) 
+		|| (form.password.value != form.rpassword.value)) {
+		msg += "Password and password confirmation do not match. <br />";
+	}
+
+	if (form.age.value == "" 
+		|| form.age.value == undefined
+		|| form.age < 13 
+		|| form.age > 150) {
+		msg += "Enter a valid age. <br />";
+	}
+
+	if (msg != "") {
+		document.getElementById("error").innerHTML=msg+"<br />";
+		return false;
+	} 
+	else {
+		return true;
+	}
 }
